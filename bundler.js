f4e87c3c3aa078b82b7d3ac2554e4a441140ba3b
cjshinning <784319417@qq.com>
2020-03-05 10:40:54 +0800

    6-6 bundler源码编写（Dependecies Graph）

--- bundler.js
+++ bundler.js
@@ -28,5 +28,29 @@ const moduleAnalyser = (filename) => {
     }
 }
 
-const moduleInfo = moduleAnalyser('./src/index.js');
-console.log(moduleInfo);
+const makeDependenciesGraph = (entry) => {
+    const entryModule = moduleAnalyser(entry);
+    const graphArray = [entryModule];
+    for(let i=0;i<graphArray.length;i++){
+        const item = graphArray[i];
+        const { dependencies } = item;
+        if(dependencies){
+            for(let j in dependencies){
+                graphArray.push(
+                    moduleAnalyser(dependencies[j])
+                );
+            }
+        }
+    }
+    const graph = {};
+    graphArray.forEach(item => {
+        graph[item.filename] = {
+            dependencies: item.dependencies,
+            code: item.code
+        }
+    })
+    return graph;
+}
+
+const graphInfo = makeDependenciesGraph('./src/index.js');
+console.log(graphInfo);
